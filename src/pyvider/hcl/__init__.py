7e22952a266042d0661507448e930fb720df4588
Tim <code@tim.life>
2025-09-05 20:00:00 -0700

    chore: update version to 0.2.0 and synchronize version files

--- src/pyvider/hcl/__init__.py
+++ src/pyvider/hcl/__init__.py
@@ -1,5 +1,7 @@
 # pyvider/hcl/__init__.py
 
+from pyvider.hcl._version import __version__
+
 from .exceptions import HclError, HclParsingError
 from .factories import create_resource_cty, create_variable_cty
 from .parser import parse_hcl_to_cty, parse_with_context
@@ -7,6 +9,7 @@ from .printer import pretty_print_cty
 from .terraform import parse_terraform_config
 
 __all__ = [
+    "__version__",
     "HclError",
     "HclParsingError",
     "create_resource_cty",
